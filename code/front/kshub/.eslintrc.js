ededd92282bdd9df5def9bf2126139e35c9d7eee
李博修 <liboxiu@dreamofalice.club>
2020-04-19 20:10:32 +0800

    config eslint

--- code/front/kshub/.eslintrc.js
+++ code/front/kshub/.eslintrc.js
@@ -1,29 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+      node: true
   },
   'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
+      'plugin:vue/essential',
+      '@vue/typescript'
   ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        mocha: true
-      }
-    }
-  ]
+  parserOptions: {
+      parser: '@typescript-eslint/parser'
+  }
 }

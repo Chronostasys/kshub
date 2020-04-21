77ac362bf67af8bafc2585536c6b56545e83fb18
泉 周 <1146673207@qq.com>
2020-04-21 11:36:50 +0800

    commit

--- code/front/kshub/src/router/index.ts
+++ code/front/kshub/src/router/index.ts
@@ -25,6 +25,14 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../components/Login/Login.vue')
+  },
+  {
+    path: '/project',
+    name: 'Project',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/project.vue')
   }
 ]
 
---
+++ code/front/kshub/src/views/project.vue
@@ -0,0 +1,145 @@
+<template>
+<div :class="modalCssClass">
+    <div class=" max-500px modal-card">
+    <div class="model-card-head field has-icons-right">
+        <h1 class=" modal-card-title ">New project
+            <span class=" clickable icon is-pulled-right"
+                title="关闭"
+                @click="close">
+                <i class=" fa fa-close"></i>
+            </span>
+        </h1>
+    </div>
+    <div class="field">
+  <label class="label">Name</label>
+    <div class="control">
+    <input class="input" type="text" placeholder="Text input">
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Username</label>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input is-success" type="text" placeholder="Text input" value="bulma">
+    <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </div>
+  <p class="help is-success">This username is available</p>
+</div>
+
+<div class="field">
+  <label class="label">Email</label>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
+    <span class="icon is-small is-left">
+      <i class="fas fa-envelope"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-exclamation-triangle"></i>
+    </span>
+  </div>
+  <p class="help is-danger">This email is invalid</p>
+</div>
+
+<div class="field">
+  <label class="label">Subject</label>
+  <div class="control">
+    <div class="select">
+      <select>
+        <option>Select dropdown</option>
+        <option>With options</option>
+      </select>
+    </div>
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Message</label>
+  <div class="control">
+    <textarea class="textarea" placeholder="Textarea"></textarea>
+  </div>
+</div>
+
+<div class="field">
+  <div class="control">
+    <label class="checkbox">
+      <input type="checkbox">
+      I agree to the <a href="#">terms and conditions</a>
+    </label>
+  </div>
+</div>
+
+<div class="field">
+  <div class="control">
+    <label class="radio">
+      <input type="radio" name="question">
+      Yes
+    </label>
+    <label class="radio">
+      <input type="radio" name="question">
+      No
+    </label>
+  </div>
+</div>
+
+<div class="field is-grouped">
+  <div class="control">
+    <button class="button is-link">Submit</button>
+  </div>
+  <div class="control">
+    <button class="button is-link is-light">Cancel</button>
+  </div>
+</div>
+</div>
+</div>
+</template>
+<script lang="ts">
+import "bulma/css/bulma.css";
+// @ is an alias to /src
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import HelloWorld from '@/components/HelloWorld.vue';
+import Axios from 'axios';
+
+@Component({
+    components:{
+        HelloWorld
+    }
+})
+export default class Login extends Vue {
+    userdata:any={
+            email: "string",
+            rememberme: true,
+            password: "string"
+        };
+    modalCssClass="is-active modal";
+    close(){
+        this.modalCssClass = 'modal';
+    }
+    login(){
+        
+        Axios.post('/api/User/LogIn',this.userdata).then((params)=>{
+            var id = params.data.id;
+            this.userdata.id=id;
+        }).catch((err)=>{
+            console.log(err);
+            alert(err);
+        })
+    }
+}
+</script>
+
+<style>
+.clickable{
+    cursor: pointer;
+}
+.max-500px {
+    max-width: 500px;
+}
+.isforgivable{
+    background-color: chartreuse;
+}
+</style>

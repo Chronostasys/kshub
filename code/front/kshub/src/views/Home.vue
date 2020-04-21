1223445c76221f09b523297ac0fb48c814cdaa92
泉 周 <1146673207@qq.com>
2020-04-21 16:23:25 +0800

    commit

--- code/front/kshub/src/views/Home.vue
+++ code/front/kshub/src/views/Home.vue
@@ -1,19 +1,24 @@
 <template>
+<div>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <Login/>
   </div>
+  <button class="button is-black">new project</button>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import Login from '../components/Login/Login.vue'
+import project from '../components/Project/project'
+import { Prop } from 'vue-property-decorator'
 
 export default {
   name: 'Home',
   components: {
     Login
-  }
+  },
 }
 </script>

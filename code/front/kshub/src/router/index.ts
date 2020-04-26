086dc46b088aecf3e7405182f8452e551bf1741f
泉 周 <1146673207@qq.com>
2020-04-26 12:13:36 +0800

    commit

--- code/front/kshub/src/router/index.ts
+++ code/front/kshub/src/router/index.ts
@@ -19,14 +19,6 @@ Vue.use(VueRouter)
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/project',
-    name: 'Project',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Project.vue')
-  },
-  {
     path: '/myproject',
     name: 'MyProject',
     // route level code-splitting

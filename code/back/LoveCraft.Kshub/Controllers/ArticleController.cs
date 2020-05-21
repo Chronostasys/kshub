8863fd3b46ecda2ae6ac1999499492ee8b9eda70
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-05-21 20:06:37 +0800

    Add ArticleController

---
+++ code/back/LoveCraft.Kshub/Controllers/ArticleController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Microsoft.Extensions.Hosting;
+using LoveCraft.Kshub.Services;
+using DocumentFormat.OpenXml.Spreadsheet;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LoveCraft.Kshub.Controllers
+{
+    [Route("api/[controller]")]
+    public class ArticleController : Controller
+    {
+        private readonly KshubService _kshubService;
+        public IMapper _mapper { get; set; }
+        public IHostEnvironment env { get; set; }
+        public ArticleController(KshubService kshubService,IMapper mapper,IHostEnvironment env)
+        {
+            _kshubService = kshubService;
+            _mapper = mapper;
+            this.env = env;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

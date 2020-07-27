54a78d7666e99f14636a349e3f9f25c650b19c52
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-07-27 18:01:55 +0800

    add dtos

--- code/back/LoveCraft.Kshub/Controllers/KsController.cs
+++ code/back/LoveCraft.Kshub/Controllers/KsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using LoveCraft.Kshub.Dto;
 using LoveCraft.Kshub.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,9 +15,21 @@ namespace LoveCraft.Kshub.Controllers
     [ApiController]
     public class KsController : Controller
     {
-        private readonly KshubService _kshubService;
-        readonly IMapper _mapper;
-        IHostEnvironment env;
+        private KshubService _kshubService { get; }
+        private IHostEnvironment _env { get; }
+        private IMapper _mapper { get; }
+        public KsController(KshubService kshubService, IHostEnvironment env, IMapper mapper)
+        {
+            _kshubService = kshubService;
+            _mapper = mapper;
+            _env = env;
+        }
+
+        [HttpPost]
+        public async ValueTask AddKsAsymc(AddKsDto addKsDto)
+        {
+
+        }
 
     }
 }

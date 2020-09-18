92fca1dd0c2aef1d0c432d1a7dae8adb2884f0af
泉 周 <1146673207@qq.com>
2020-09-18 22:02:06 +0800

    commit

--- code/front/kshub/src/views/MyProject.vue
+++ code/front/kshub/src/views/MyProject.vue
@@ -2,7 +2,7 @@
     <div class="container">
       <div class="columns">
         <div class="column is-one-quarter">
-          <div class="subtitle is-hidden-touch">My Course</div>
+          <div class="subtitle is-hidden-touch">学校</div>
           <ul>
             <li :key="item.id" class="menu-list" v-for="item in items">
               <a href="#" :class="item.ishighlight?'is-active':''" @click="()=>highlight(item)" :key="item1">

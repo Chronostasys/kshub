a066beb15353c54f8a1fac4752f746ad0a977c61
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-09-21 16:20:00 +0800

    Refactor Course Model

--- code/back/LoveCraft.Kshub/Models/Course.cs
+++ code/back/LoveCraft.Kshub/Models/Course.cs
@@ -9,19 +9,23 @@ namespace LoveCraft.Kshub.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
-
         public string CoverUrl { get; set; }
-
-        public Guid BelongedCollegeId { get; set; }
-        
-        //All a few teacher to manager the same course 
-        public List<Guid> TeachersId { get; set; }
-
-        public List<Guid> StudentsID { get; set; }
-        
         //specify the final score rating
         public Dictionary<string,double> ScoreRating { get; set; }
         public string SearchAbleString { get; set; }
 
+        //This prosperity will store all Relationship 
+        //such as Belonged University-College,the CollegeAdmin,
+        //Teacher and the students who belong this course
+        public List<GroupMember> Groups { get; set; }
+        //Ks In this Course
+        public List<Guid> KsList { get; set; }
+        //-------------------------------------------------
+        //the following prosperies will be removed when Groups finished 
+        public Guid BelongedCollegeId { get; set; }
+        //All a few teacher to manager the same course 
+        public List<Guid> TeachersId { get; set; }
+        public List<Guid> StudentsID { get; set; }
+        //---------------------------------------------------
     }
 }
---
+++ code/back/LoveCraft.Kshub/Models/GroupMember.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoveCraft.Kshub.Models
+{
+    /// <summary>
+    /// GroupMember is designed for storing relationship in Course
+    /// It contains University,College,Major,Grade,Class and StuId
+    /// </summary>
+    public class GroupMember
+    {
+        public string University { get; set; }
+        public string College { get; set; }
+        public string Major { get; set; }
+        public int Grade { get; set; }
+        public string Class { get; set; }
+        public Guid StuId { get; set; }
+    }
+}

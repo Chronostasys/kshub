0647a731759c65794f480ffa6af8fa4aa9d11acc
李博修 <liboxiu@dreamofalice.club>
2021-02-07 11:06:39 +0800

    fix config

--- code/front/kshub/vue.config.js
+++ code/front/kshub/vue.config.js
@@ -25,18 +25,18 @@ module.exports = {
     integrity: false,
     // 反向代理
     devServer: {
-        http2:true,
+        // http2:true,
         https:false,
         proxy: {
             '/api': {
                 // 要访问的跨域的域名
-                target: 'https://localhost:5001',
+                target: 'http://localhost:5000',
                 // target: 'https://limfx.pro',
                 ws: true, // 是否启用websockets
                 pathRewrite: { '^/frontend': '' },
                 secure: true, // 使用的是http协议则设置为false，https协议则设置为true
                 // 开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样客户端端和服务端进行数据的交互就不会有跨域问题
-                changOrigin: false,
+                changOrigin: true,
                 cookieDomainRewrite: "localhost"
             },
             // '/img': {
